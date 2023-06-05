64c7e6349a5cc4d6237d475d82c0042e0651d749
HorizonCode <horizoncode88@gmail.com>
2023-06-05 15:11:25 +0200

    avoid multiple file names

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { HTTPServer } from "https://deno.land/x/rapid@v0.2.2/mod.ts";
 import * as cryptoString from "https://deno.land/x/crypto_random_string@1.1.0/mod.ts";
 import * as path from "https://deno.land/std@0.190.0/path/mod.ts";
+import { fileExists } from "./fileUtils.ts";
 
 const webApp = new HTTPServer(false);
 
 webApp.post("/sharex/upload", async (request, _reply) => {
-  const randomStr = cryptoString.cryptoRandomString({ length: 16 });
-  const fileUrl = `//${request.header("host")}/i/${randomStr}.png`;
+  let randomStr = cryptoString.cryptoRandomString({ length: 16 });
+  const fileUrl = `http://${request.header("host")}/i/${randomStr}.png`;
   const fileBlob = await request.blob();
   if (fileBlob.size > 104857600) return "file exceeded 100MB file size!";
   if (!fileBlob.type.includes("/")) return "invalid filetype!";
@@ -15,11 +16,20 @@ webApp.post("/sharex/upload", async (request, _reply) => {
   if (mediaType != "image") return "invalid filetype!";
   const mediaEncoding = fileArgs[1];
   if (fileBlob) {
+    //Avoid same file names, lmao
+    while (await fileExists(path.join(Deno.cwd(), "pub", `${randomStr}.${mediaEncoding}`))) {
+      console.log("file exists, generating new random string");
+      randomStr = cryptoString.cryptoRandomString({ length: 16 });
+    }
+
+    console.log("writing data...");
+
     try {
       await Deno.writeFile(
         path.join(Deno.cwd(), "pub", `${randomStr}.${mediaEncoding}`),
         fileBlob.stream(),
       );
+      console.log(`uploaded file ${randomStr}.${mediaEncoding}`)
       return fileUrl;
     } catch (_err) {
       return "error while processing file";
