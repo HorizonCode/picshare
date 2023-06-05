2e9c37be4b5889fed5ba902d57a45acf67e38604
HorizonCode <horizoncode88@gmail.com>
2023-06-05 16:11:47 +0200

    add text sharing support

--- index.ts
+++ index.ts
@@ -6,35 +6,60 @@ import { fileExists } from "./fileUtils.ts";
 const webApp = new HTTPServer(false);
 
 webApp.post("/sharex/upload", async (request, _reply) => {
-  let randomStr = cryptoString.cryptoRandomString({ length: 16 });
-  const fileUrl = `http://${request.header("host")}/i/${randomStr}.png`;
   const fileBlob = await request.blob();
-  if (fileBlob.size > 104857600) return "file exceeded 100MB file size!";
-  if (!fileBlob.type.includes("/")) return "invalid filetype!";
-  const fileArgs = fileBlob.type.split("/");
-  const mediaType = fileArgs[0];
-  if (mediaType != "image") return "invalid filetype!";
-  const mediaEncoding = fileArgs[1];
   if (fileBlob) {
-    //Avoid same file names, lmao
-    while (await fileExists(path.join(Deno.cwd(), "pub", `${randomStr}.${mediaEncoding}`))) {
-      console.log("file exists, generating new random string");
-      randomStr = cryptoString.cryptoRandomString({ length: 16 });
+    if (fileBlob.size > 104857600) return "file exceeded 100MB file size!";
+    if (!fileBlob.type.includes("/")) return `invalid filetype (${fileBlob.type})!`;
+    const fileArgs = fileBlob.type.split("/");
+    const mediaType = fileArgs[0];
+    const mediaEncoding = fileArgs[1];
+    switch (mediaType) {
+      case "text": {
+        let randomStr = cryptoString.cryptoRandomString({ length: 16 });
+        const fileUrl = `http://${request.header("host")}/data/${randomStr}.txt`;
+        while (await fileExists(path.join(Deno.cwd(), "pub", `${randomStr}.txt`))) {
+          console.log("file exists, generating new random string");
+          randomStr = cryptoString.cryptoRandomString({ length: 16 });
+        }
+        const text = await fileBlob.text();
+        console.log("writing data...");
+        try {
+          await Deno.writeTextFile(
+            path.join(Deno.cwd(), "pub", `${randomStr}.txt`),
+            text,
+          );
+          console.log(`uploaded file ${randomStr}.txt`)
+          return fileUrl;
+        } catch (err) {
+          console.log(err)
+          return "error while processing file";
+        }
+      }
+      case "image": {
+        let randomStr = cryptoString.cryptoRandomString({ length: 16 });
+        const fileUrl = `http://${request.header("host")}/data/${randomStr}.${mediaEncoding}`;
+        //Avoid same file names, lmao
+        while (await fileExists(path.join(Deno.cwd(), "pub", `${randomStr}.${mediaEncoding}`))) {
+          console.log("file exists, generating new random string");
+          randomStr = cryptoString.cryptoRandomString({ length: 16 });
+        }
+        console.log("writing data...");
+        try {
+          await Deno.writeFile(
+            path.join(Deno.cwd(), "pub", `${randomStr}.${mediaEncoding}`),
+            fileBlob.stream(),
+          );
+          console.log(`uploaded file ${randomStr}.${mediaEncoding}`)
+          return fileUrl;
+        } catch (err) {
+          console.log(err)
+          return "error while processing file";
+        }
+      }
+      default:
+        return `invalid filetype(${fileBlob.type})!`;
     }
 
-    console.log("writing data...");
-
-    try {
-      await Deno.writeFile(
-        path.join(Deno.cwd(), "pub", `${randomStr}.${mediaEncoding}`),
-        fileBlob.stream(),
-      );
-      console.log(`uploaded file ${randomStr}.${mediaEncoding}`)
-      return fileUrl;
-    } catch (err) {
-      console.log(err)
-      return "error while processing file";
-    }
   }
   return "error";
 });
@@ -42,5 +67,5 @@ webApp.post("/sharex/upload", async (request, _reply) => {
 webApp.listen({
   port: 8081,
   staticLocalDir: "/pub",
-  staticServePath: "/i",
+  staticServePath: "/data",
 });
